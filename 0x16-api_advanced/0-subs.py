9b5386e0adab2a260e0b3db5ac1efd1c3a4070aa
CarolinaHV <carolina.hv02@gmail.com>
2021-06-14 00:52:52 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+"""
+Finds the number of subscribers in a subreddit
+
+"""
+
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    ''' Finds the number of subscribers in a subreddit '''
-
     header = {'User-Agent':  'Chrome/91.0.4472.77 Safari/537.36'}
     url = "https://api.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=header)
-
     if response.status_code == 404:
         return 0
     subscribers = response.json().get('data').get('subscribers')
